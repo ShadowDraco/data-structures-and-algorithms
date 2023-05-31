a3dc83a523b0d678863eb524d68c1d015c573bd4
Ethan Storm <stormethan10@gmail.com>
2023-05-31 15:10:49 -0700

    finished documentation, challenge, and testing

---
+++ javascript/stack-and-queue/animal-shelter/index.js
@@ -0,0 +1,37 @@
+const { Queue } = require("../index");
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class AnimalShelter {
+  constructor() {
+    this.queue = new Queue();
+  }
+
+  enqueue(name, type) {
+    if (type === "dog") {
+      this.queue.enqueue(new Dog(name));
+    } else if (type === "cat") {
+      this.queue.enqueue(new Cat(name));
+    } else {
+      throw new Error("We don't take those here");
+    }
+  }
+  dequeue(pref) {
+    if (pref === "dog") return new Dog("puppy");
+    if (pref === "cat") return new Cat("kitten");
+
+    return this.queue.dequeue();
+  } // end dequeue
+} // end class
+
+module.exports = AnimalShelter;
